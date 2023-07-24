922aafe7c66159b94e1a647a1e9087ccb13c1ab8
Dylan Vu <dylanvu9@gmail.com>
2023-07-23 21:54:07 -0700

    create a collapsible component

---
+++ js/collapsible.js
@@ -0,0 +1,96 @@
+/** A webpage collapsible component
+ * @example <script src="./js/collapsible.js" title="Click to Reveal Answer" content="290Ω"></script>
+ */
+function createCollapsible() {
+
+    // title: what is seen at the top of the collapsible
+    const title = document.currentScript.getAttribute('title');
+    // content: what is seen inside the collapsible
+    // could be HTML
+    const content = document.currentScript.getAttribute('content');
+
+    // a collapsible needs to point to a unique id for the bootstrap collapsible function. Dynamically generate an id based on the collapsible title and contents, hoping that it's unique across the page.
+    // testing every collapsible is highly recommended when utilizing a collapsible component
+
+    const collapsibleID = generateCollapsibleID(title, content);
+        
+    // check to see if the collapsible built will have a unique ID
+    const isUniqueID = checkIfUniqueID(collapsibleID);
+
+    if (!isUniqueID) {
+        // notify in the console of an error
+        console.error(`Collapsible with title: "${title}" and contents: "${content}" is the same across another collapsible in this page. Modify the title or contents to make the ID "${collapsibleID}" unique.`);
+    }
+
+    const accordionItemID = `${collapsibleID}1`
+
+
+    // utilize the bootstrap accordion component as an easy to implement collapsible
+    document.write(`
+    <div class="accordion mb-5" id="${collapsibleID}">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#${accordionItemID}" aria-expanded="true" aria-controls="${accordionItemID}">
+                    ${title}
+                </button>
+            </h2>
+            <div id="${accordionItemID}" class="accordion-collapse collapse" data-bs-parent="#${collapsibleID}">
+                <div class="accordion-body text-center">
+                    ${content}
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+}
+
+// TODO: find a more clever way to generate unique IDs for the collapsible component?
+/**
+ * 
+ * generate a valid string that can be used 
+ * @param {string} title 
+ * @param {string} content 
+ * @returns {string}
+ */
+function generateCollapsibleID(title, content) {
+    /* a valid HTML ID:
+        1. must start with a letter
+        2. can have letters (a-z, A-Z), digits (0-9), hyphens (-), and underscores (_)
+        3. **must be unique in the HTML document**
+    */
+    
+    // concatenate the title and content together as the base id
+    const baseID = `${title}-${content}`;
+    
+    // test the length
+    if (baseID.length == 0) {
+        // notify error and give this collapsible a dummy ID and hope it works
+        const dummyID = "dummy_collapsible_id";
+        console.error(`Title and content of collapsible is blank. Giving the collapsible a dummy id: ${dummyID}`);
+        return dummyID;
+    }
+    
+    // utilize some regex to replace all characters that are not letters, digits, hyphens, or underscores with underscores
+    let cleaned = baseID.replace(/[^a-zA-Z0-9-_]/g, '_');
+
+    // if the id starts with a number
+    if (!isNaN(cleaned[0])) {
+        // add an underscore in the front
+        cleaned = `_${cleaned}`;
+    }
+
+    return cleaned;
+}
+
+/**
+ * queries the document for an element with the stated id, returning true if it's unique and
+ * @param {string} id 
+ * @returns {boolean}
+ */
+function checkIfUniqueID(id) {
+    return document.getElementById(id) ? false : true;
+}
+
+// run the collapsible code
+createCollapsible();
