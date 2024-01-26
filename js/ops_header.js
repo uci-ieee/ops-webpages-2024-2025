13e9d6b3d21ebe3429d53f9524ffe34b62be6365
bcbielecki <bcbielecki@gmail.com>
2024-01-25 19:17:30 -0800

    yahtzee

--- js/ops_header.js
+++ js/ops_header.js
@@ -28,6 +28,7 @@ document.write(`
                             <li><a class="dropdown-item" href="./project_3.html">Project 3: C++ Programming</a></li>
                             <li><a class="dropdown-item" href="./project_4.html">Project 4: RGB LED Dimmer</a></li>
                             <li><a class="dropdown-item" href="./project_5.html">Project 5: iPoduino v2.0</a></li>
+                            <li><a class="dropdown-item" href="./project_6.html">Project 6: Weather Station</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
